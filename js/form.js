0b4062d52b4e0425145d7c5a6fba123a50b024cc
K-Elb <kareemkimo711@hotmail.com>
2026-02-06 21:53:13 +0400

    updated video and form and added reviews

--- js/form.js
+++ js/form.js
@@ -1,17 +1,16 @@
   document.getElementById('RegisterForm').addEventListener('submit', function(e) {
     e.preventDefault();
     
-    const formURL = 'https://docs.google.com/forms/d/e/1FAIpQLSezCDYofsYwe9LAIlInhzoQy3tmEc1vYX_So8zFj_H2M1Uzxw/formResponse';
+    const formURL = 'https://docs.google.com/forms/d/e/1FAIpQLSeRFlM1VJwvst6N6ayc7o7MV6IaNe7GzBb4DQfeMiJscY1Xrw/formResponse';
     
     const choiceValue = document.querySelector('input[name="choice"]:checked').value;
     
     const formData = new FormData();
-    formData.append('entry.2005620554', document.getElementById('name').value);
-    formData.append('entry.839337160', document.getElementById('school').value);
-    formData.append('entry.1166974658', document.getElementById('number').value);
-    formData.append('entry.839337160', document.getElementById('email').value);
-    
-    formData.append('entry.1032903721', choiceValue);
+    formData.append('entry.322101604', document.getElementById('name').value);
+    formData.append('entry.726900688', document.getElementById('school').value);
+    formData.append('entry.1296923209', document.getElementById('number').value);
+    formData.append('entry.1046531051', document.getElementById('email').value);
+    formData.append('entry.224510475', choiceValue);
     
     fetch(formURL, {
         method: 'POST',
